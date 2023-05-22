7afb26a5685ef1272ca080cc40ef2c314bf3f5a6
avitej-iyer <iyeravitej2@gmail.com>
2023-05-22 19:39:49 +0530

    May 22

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         cur_sum = 0
+        # iter through nums with lower limit
         max_sum = -999999
         for x in range(0, len(nums)):
             cur_sum += nums[x]
