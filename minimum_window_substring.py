2f48ad8966d68de1b44e6559034fceada874d34c
avitej-iyer <iyeravitej2@gmail.com>
2023-06-14 13:06:05 +0530

    June 14

--- minimum_window_substring.py
+++ minimum_window_substring.py
@@ -1,45 +1,39 @@
-def minWindow(s: str, t: str) -> str:
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
 
-        if len(s) < len(t):
-            return ""
+        # leaving it as counter objects and not dicts
+        # because of comparison below
+        s_dict, t_dict = collections.Counter(), (collections.Counter(t))
 
-        fast, slow = 0, -1
+        # defining a "slow" pointer here, fast pointer is the 
+        # loop iterator variable
+        l, results = 0, ""
 
-        t_dict={}
-        s_dict={}
+        for r in range(len(s)):
+            s_dict[s[r]] += 1
 
-        # initial hashmap contruction for keeping track of letter counts in t
-        for x in t:
-            if x in t_dict:
-                t_dict[x] += 1
-            else:
-                t_dict[x] = 1
+            # the & operator between two counter objects gives the 
+            # intersection of objects
+            # use this to shrink the window as needed
+            while (s_dict & t_dict == t_dict):
+                if len(s[l:r+1]) < len(results) or results == "":
+                    results = s[l:r+1]
+                s_dict[s[l]] -= 1
+                l+= 1
+
+        return results            
+
+
+
+
+    
         
-        ans = "1"
 
-        while fast > slow and fast < len(s):
 
-            if s[fast] in s_dict:
-                s_dict[s[fast]] += 1
-            else:
-                s_dict[s[fast]] = 1
 
-            print("Outer : ", s_dict)
-            
-            while t_dict.items() <= s_dict.items():
-                if ans == "1":
-                    ans = s[slow:fast+1]
-                else:    
-                    ans = min(ans, s[slow:fast+1], key = len)
 
-                print(ans)
-                
-                s_dict[s[slow]] -= 1
-                print("Inner : ", s_dict)
-                slow += 1
-            
-            fast += 1
 
-        return ans
 
-minWindow("ADOBECODEBANC", "ABC")
+        
+
+
