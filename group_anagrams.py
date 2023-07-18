d2cba3c9fccc00b548ac97334de6234e9e072e3e
avitej-iyer <iyeravitej2@gmail.com>
2023-07-19 02:11:03 +0530

    July 19

--- group_anagrams.py
+++ group_anagrams.py
@@ -4,6 +4,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = collections.defaultdict(str)
         for x in strs:
+            # you could alternatively jus tuse the sorted word
+            # as the key, would probably cut down on runtime for small words
             letter_count = tuple(sorted(collections.Counter(x).items()))
             if letter_count not in anagrams:
                 anagrams[letter_count] = [x]
