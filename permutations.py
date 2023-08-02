dd75ffb60b9eb72a206c44c914595651eea666f4
avitej-iyer <iyeravitej2@gmail.com>
2023-08-02 15:11:00 +0530

    August 2

--- permutations.py
+++ permutations.py
@@ -15,4 +15,24 @@ class Solution:
                     
 
         backtracking([])
-        return ret    
+        return ret  
+
+###
+# Below is the in-place backtracking solution I did for the 
+# daily leetcode challenge
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         ret = []
+#         def backtrack(i, nums):
+#             if i == len(nums):
+#                 ret.append(nums[:])
+#                 return
+#             for j in range(i, len(nums)):
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 backtrack(i+1, nums)
+#                 nums[j], nums[i] = nums[i], nums[j]
+
+#         backtrack(0, nums)        
+
+#         return ret    
+  
